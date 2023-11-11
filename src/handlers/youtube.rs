56efd396a519eeb67f4becbd6d51f3d74ce66407
The Noah <thenoahbz@gmail.com>
2023-11-11 17:56:48 -0600

    feat: add YouTube Shorts

--- src/handlers/youtube.rs
+++ src/handlers/youtube.rs
@@ -8,6 +8,10 @@ impl Handler for YouTubeHandler {
   }
 
   fn handle(&self, path: &[&str]) -> Option<String> {
+    if path.len() == 2 && path[0] == "shorts" {
+      return Some(format!("https://www.youtube.com/shorts/{}", path[1].split("?").collect::<Vec<&str>>()[0]));
+    }
+
     if path.len() != 1 || !path[0].starts_with("watch?v=") {
       return None;
     }
@@ -40,5 +44,15 @@ mod tests {
 
     let result = handler.handle(&path);
     assert_eq!(result, Some("https://youtu.be/dQw4w9WgXcQ".to_string()));
+  }
+
+  #[test]
+  fn test_short() {
+    let handler = YouTubeHandler {};
+    let url = "https://www.youtube.com/shorts/-ZDi3lBLHVw";
+    let path = &url.split("/").collect::<Vec<&str>>()[3..];
+
+    let result = handler.handle(&path);
+    assert_eq!(result, Some("https://www.youtube.com/shorts/-ZDi3lBLHVw".to_string()));
   }
 }
